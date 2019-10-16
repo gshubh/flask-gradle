e7e0396128a8e74aff263614a838e960cc488f74
gshubh <mg24369@gmail.com>
2019-10-16 15:21:34 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flask_gradle',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='humour_infinite',
+    author_email='',
+    description=''
+)
